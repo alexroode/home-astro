e13c1924b1ec1ac4f21305498c873eebabad426b
Alexander Roode <alexroode@gmail.com>
2024-03-21 16:06:10 -0700

    Reindent some files

--- src/types.ts
+++ src/types.ts
@@ -6,55 +6,55 @@ export interface ContactRequest {
 }
 
 export interface MusicLibrary {
-  readonly categories: Category[];
-  readonly pieces: Piece[];
+    readonly categories: Category[];
+    readonly pieces: Piece[];
 }
 
 export interface Category {
-  readonly name: string;
-  readonly id: string;
-  readonly description: string;
+    readonly name: string;
+    readonly id: string;
+    readonly description: string;
 }
 
 export interface Piece {
-  readonly categoryId: string;
-  readonly id: string;
-  readonly instrumentation: string;
-  readonly subtitle: string;
-  readonly title: string;
-  readonly accolades?: string;
-  readonly description?: string;
-  readonly date: moment.Moment;
-  readonly revisionDate?: moment.Moment;
-  readonly duration: number;
-  readonly grade?: number;
-  readonly scores: Document[];
-  readonly audio: AudioFile[];
-  readonly performances?: Performance[];
-  readonly products?: StripePriceReference[];
+    readonly categoryId: string;
+    readonly id: string;
+    readonly instrumentation: string;
+    readonly subtitle: string;
+    readonly title: string;
+    readonly accolades?: string;
+    readonly description?: string;
+    readonly date: moment.Moment;
+    readonly revisionDate?: moment.Moment;
+    readonly duration: number;
+    readonly grade?: number;
+    readonly scores: Document[];
+    readonly audio: AudioFile[];
+    readonly performances?: Performance[];
+    readonly products?: StripePriceReference[];
 }
 
 export interface Document {
-  readonly url: string;
-  readonly title: string;
+    readonly url: string;
+    readonly title: string;
 }
 
 export interface AudioFile {
-  readonly url: string;
-  readonly title: string;
+    readonly url: string;
+    readonly title: string;
 }
 
 export interface Performance {
-  readonly date: moment.Moment;
-  readonly venue: string;
-  readonly city: string;
-  readonly worldPremiere?: boolean;
-  readonly performers?: string[];
-  readonly youtubeVideoId?: string;
+    readonly date: moment.Moment;
+    readonly venue: string;
+    readonly city: string;
+    readonly worldPremiere?: boolean;
+    readonly performers?: string[];
+    readonly youtubeVideoId?: string;
 }
 
 export interface StripePriceReference {
-  readonly priceId: string;
-  readonly name: string;
-  readonly prod: boolean;
+    readonly priceId: string;
+    readonly name: string;
+    readonly prod: boolean;
 }
