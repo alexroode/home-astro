cbb87a7dac809d98a3c7d7acdb0d8792e00c8120
Alexander Roode <alexroode@gmail.com>
2024-03-21 14:35:10 -0700

    Add remaining piece layout

---
+++ src/utils/formatters.ts
@@ -0,0 +1,39 @@
+import { filesize } from "filesize";
+
+export function formatDuration(totalSeconds: number) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    
+    let formattedString = minutes + "′";
+    if (seconds !== 0) {
+        formattedString += " " + seconds + "″";
+    }
+    return formattedString;
+}
+
+export function formatGrade(grade: number) {
+    let descriptor = "";
+    switch (grade) {
+        case 4:
+        descriptor = "Medium-Difficult";
+        break;
+        case 4.5:
+        descriptor = "Medium-Difficult";
+        break;
+        case 5:
+        descriptor = "Difficult";
+        break;
+        case 6:
+        descriptor = "Professional";
+        break;
+    }
+    
+    if (descriptor) {
+        return grade + ` (${descriptor})`;
+    }
+    return grade;
+}
+
+export function formatFilesize(filesizeString: string) {
+    return filesize(+filesizeString);
+}
