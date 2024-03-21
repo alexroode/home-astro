748db72c3aa0eab35c2510269c574f9afe14f584
Alexander Roode <alexroode@gmail.com>
2024-03-21 13:57:55 -0700

    Add category pages

---
+++ src/services/music-service.ts
@@ -0,0 +1,96 @@
+import moment from "moment";
+import type { MusicLibrary, Piece, Category } from "../types.js";
+import musicJson from "./music.json";
+
+function notFound(): Promise<any> {
+    return Promise.reject(null);
+}
+
+function mapJsonDate(dateString: string): moment.Moment;
+function mapJsonDate(dateString?: string): moment.Moment | undefined;
+
+function mapJsonDate(dateString?: string): moment.Moment | undefined {
+    if (!dateString) {
+        return undefined;
+    }
+    return moment(dateString, "YYYY/MM/DD");
+}
+
+async function loadDescription(piece: any) {
+    /*const filePath = path.join(import.meta.dirname, "pieces", piece.id + ".md");
+    try {
+        const markdown = await readFile(filePath, "utf8");
+        const description = marked(markdown);
+        piece.description = description;
+    } catch {
+        piece.description = "";
+    }*/
+    piece.description = "";
+}
+
+function dateReviver(key: keyof(Piece) | keyof(Performance), value: any) {
+    if (key === "revisionDate" || key === "date") {
+        return mapJsonDate(value);
+    }
+    return value;
+}
+
+export class MusicService {
+    private music: MusicLibrary = {
+        categories: musicJson.categories,
+        pieces: musicJson.pieces.map(piece => ({
+            ...piece,
+            date: mapJsonDate(piece.date),
+            revisionDate: mapJsonDate(piece.revisionDate),
+            performances: piece.performances?.map(performance => ({
+                ...performance,
+                date: mapJsonDate(performance.date)
+            }))
+        }))
+    }
+    
+    private sortPieces(a: Piece, b: Piece): number {
+        const aDate = a.revisionDate || a.date;
+        const bDate = b.revisionDate || b.date;
+        return bDate.diff(aDate);
+    }
+    
+    async getLibrary(): Promise<MusicLibrary> {
+        return Promise.resolve(this.music);
+    }
+    
+    async getInCategory(categoryId: string): Promise<Piece[]> {
+        const library = await this.getLibrary();
+        const pieces = library.pieces
+        .filter(p => p.categoryId === categoryId)
+        .sort(this.sortPieces);
+        return pieces;
+    }
+    
+    async findCategory(id: string): Promise<Category> {
+        const library = await this.getLibrary();
+        const category = library.categories.find(c => c.id === id);
+        if (category) {
+            return category;
+        }
+        return notFound();
+    }
+    
+    async findPiece(id: string, categoryId: string): Promise<Piece> {
+        const library = await this.getLibrary();
+        const piece = library.pieces.find(p => p.id === id && p.categoryId === categoryId);
+        if (piece) {
+            return piece;
+        }
+        return notFound();
+    }
+    
+    async getLatest(count: number): Promise<Piece[]> {
+        const library = await this.getLibrary();
+        return [...library.pieces]
+        .sort(this.sortPieces)
+        .slice(0, count);
+    }
+}
+
+export const Music = new MusicService();
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,57 @@ export interface ContactRequest {
     message: string;
     recaptchaResponse?: string;
 }
+
+export interface MusicLibrary {
+  readonly categories: Category[];
+  readonly pieces: Piece[];
+}
+
+export interface Category {
+  readonly name: string;
+  readonly id: string;
+  readonly description: string;
+}
+
+export interface Piece {
+  readonly categoryId: string;
+  readonly id: string;
+  readonly instrumentation: string;
+  readonly subtitle: string;
+  readonly title: string;
+  readonly accolades?: string;
+  readonly description?: string;
+  readonly date: moment.Moment;
+  readonly revisionDate?: moment.Moment;
+  readonly duration: number;
+  readonly grade?: number;
+  readonly scores: Document[];
+  readonly audio: AudioFile[];
+  readonly performances?: Performance[];
+  readonly products?: StripePriceReference[];
+}
+
+export interface Document {
+  readonly url: string;
+  readonly title: string;
+}
+
+export interface AudioFile {
+  readonly url: string;
+  readonly title: string;
+}
+
+export interface Performance {
+  readonly date: moment.Moment;
+  readonly venue: string;
+  readonly city: string;
+  readonly worldPremiere?: boolean;
+  readonly performers?: string[];
+  readonly youtubeVideoId?: string;
+}
+
+export interface StripePriceReference {
+  readonly priceId: string;
+  readonly name: string;
+  readonly prod: boolean;
+}
