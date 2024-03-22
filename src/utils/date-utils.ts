bc27b522460a03f40af3f7e27f3a022cb64a9b8e
alexroode <alexroode@gmail.com>
2024-03-21 19:34:12 -0700

    Add music (#1)
    * Delete unused ts entry
    
    * Add category pages
    
    * Add remaining piece layout
    
    * Add piece descriptions
    
    * Add music index
    
    * Add home page
    
    * Reindent some files
    
    * Fix some check issues

---
+++ src/utils/date-utils.ts
@@ -0,0 +1,32 @@
+import moment from "moment";
+import type { Piece } from "../types.js";
+
+function formatYear(moment: moment.Moment): string {
+    if (!moment) {
+        return "";
+    }
+    return moment.format("YYYY");
+}
+
+function formatDate(moment: moment.Moment): string {
+    if (!moment) {
+        return "";
+    }
+    return moment.format("MMMM D, YYYY");
+}
+
+function formatPieceYear(piece: Piece): string {
+    let result = formatYear(piece.date);
+    
+    if (piece.revisionDate) {
+        result += " / " + formatYear(piece.revisionDate);
+    }
+    
+    return result;
+}
+
+function isDateInPast(date: moment.Moment) {
+    return moment().isAfter(date);
+}
+
+export { formatYear, formatDate, formatPieceYear, isDateInPast };
