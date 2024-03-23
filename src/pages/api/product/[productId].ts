2378d4ae5725e950f9c9cbdce7d326eac3021348
Alexander Roode <alexroode@gmail.com>
2024-03-23 16:06:55 -0700

    Disable prerender for an API route

--- src/pages/api/product/[productId].ts
+++ src/pages/api/product/[productId].ts
@@ -1,20 +1,14 @@
 import type { APIRoute } from "astro";
-import { getAllProducts } from "../../../utils/product-utils";
+import { getProduct } from "../../../utils/product-utils";
 
-const products = await getAllProducts(import.meta.env.PROD);
-
-export async function getStaticPaths() {
-    return products.map(product => ({
-        params: { productId: product.id }
-    }));
-}
+export const prerender = false;
 
 export const GET: APIRoute = async ({ params }) => {
     if (!params.productId) {
         return new Response(null, { status: 404 });
     }
 
-    const product = products.find(product => product.id === params.productId);
+    const product = getProduct(params.productId);
 
     if (!product) {
         return new Response(null, { status: 404 });
